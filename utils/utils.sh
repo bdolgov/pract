9b424a76e3b6a168737ab0ca6f7804d5509f7c72
Boris Dolgov <boris@dolgov.name>
2012-11-08 14:51:18 +0400

    Added utils

---
+++ utils/utils.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+do_get_deps() {
+	src=$1
+	[ ! -e "$src" ] && return
+	(grep "^#include" $src | while read dep; do
+		if [[ "$dep" =~ $regex ]]; then
+			echo "${BASH_REMATCH[1]}"
+			do_get_deps ${BASH_REMATCH[1]}
+		fi
+	done)
+}
+
+get_deps() {
+	local regex="#include\s*\"(.*)\""
+	do_get_deps $1 | sort | uniq | xargs echo -n
+}
+
