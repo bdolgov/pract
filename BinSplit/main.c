58609fc7625ffbce863f3a4eca051f7cd1fb4c89
Boris Dolgov <boris@dolgov.name>
2012-11-08 16:01:08 +0400

    Added WA solution for BinSplit

---
+++ BinSplit/main.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <limits.h>
+#include <stdlib.h>
+typedef struct FHeader
+{
+	char magic[4];
+	int32_t partnum;
+} FHeader;
+
+typedef struct FSection
+{
+	int32_t offset;
+	int32_t size;
+} FSection;
+
+int min(int a, int b)
+{
+	return a < b ? a : b;
+}
+
+void my_sendfile(FILE* in, FILE* out, int size)
+{
+	char buf[4096];
+	while (size >= sizeof(buf))
+	{
+		fread(buf, 1, sizeof(buf), in);
+		fwrite(buf, 1, sizeof(buf), out);
+		size -= sizeof(buf);
+	}
+	fread(buf, 1, size, in);
+	fwrite(buf, 1, size, out);
+}
+
+int main(int argc, char** argv)
+{
+	if (argc != 3) return 1;
+	int n = atoi(argv[2]);
+
+	FILE *in = fopen(argv[1], "rb");
+
+	FHeader h1;
+	fread(&h1, sizeof(FHeader), 1, in);
+
+
+	FSection *sections = malloc(sizeof(FSection) * h1.partnum);
+	FSection *newsect = malloc(sizeof(FSection) * h1.partnum);
+	fseek(in, -h1.partnum * sizeof(FSection), SEEK_END);
+	fread(sections, sizeof(FSection), h1.partnum, in);
+
+	int cnt = h1.partnum;
+	int sectidx = 0;
+	for (int i = 0; i < min(n, cnt); ++i)
+	{
+		char file[sizeof("file.bin-4294967295")];
+		snprintf(file, sizeof(file), "file.bin-%d", i + 1);
+		FILE *out = fopen(file, "wb");
+		h1.partnum = cnt / n + (i < cnt % n);
+		fwrite(&h1, sizeof(FHeader), 1, out);
+		for (int j = 0; j < cnt / n + (i < cnt % n); ++j)
+		{
+			newsect[j].offset = ftell(out); //fseek(out, 0, SEEK_CUR);
+			newsect[j].size = sections[sectidx].size;
+			fseek(in, sections[sectidx].offset, SEEK_SET);
+			my_sendfile(in, out, sections[sectidx].size);
+			++sectidx;
+		}
+		for (int j = 0; j < cnt / n + (i < cnt % n); ++j)
+		{
+			fwrite(newsect + j, sizeof(FSection), 1, out);
+		}
+		fclose(out);
+	}
+	fclose(in);
+	free(newsect);
+	free(sections);
+}
