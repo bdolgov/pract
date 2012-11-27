cfab827e7ac0376370e37617173eb3742e3750b9
Boris Dolgov <boris@dolgov.name>
2012-11-27 11:27:42 +0400

    Rewrite m07-1

--- m07-1/main.c
+++ m07-1/main.c
@@ -1,59 +1,57 @@
-#include "vector.h"
-#include "process.h"
-
 #include <unistd.h>
-#include <stdlib.h>
 #include <fcntl.h>
+#include <sys/wait.h>
 
 int main(int ac, char** av)
 {
-	process_init();
+	if (ac != 7) return 1;
+	char *pr1 = av[1], *pr2 = av[2], *pr3 = av[3];
+	char *a1 = av[4], *a2 = av[5], *a3 = av[7];
 
-	int pipes[2]; 
-	pipe(pipes);
+	int pipes[2]; pipe(pipes);
 
-	int a1 = open(av[4], O_RDONLY);
-	int a3 = creat(av[6], 0644);
-	vector_push_back(opened_fds, pipes[0]);
-	vector_push_back(opened_fds, pipes[1]);
-	vector_push_back(opened_fds, a1);
-	vector_push_back(opened_fds, a3);
-
-	process_t *pr1 = process_new(), *pr2 = process_new(), *pr3 = process_new();
-	process_set_args(pr1, 1, av[1]);
-	process_set_args(pr2, 1, av[2]);
-	process_set_args(pr3, 2, av[3], av[5]);
-	
-	ioredirect_t r;
-	r.f = pipes[1];
-	r.fd = 1;
-	vector_push_back(pr1->redirs, r);
-	vector_push_back(pr2->redirs, r);
-	r.f = a3;
-	vector_push_back(pr3->redirs, r);
-	r.f = pipes[0];
-	r.fd = 0;
-	vector_push_back(pr3->redirs, r);
-	r.f = a1;
-	vector_push_back(pr2->redirs, r);
-
-	process_start(pr1);
-	process_start(pr3);
-
-	while (pr1->state != EXITED)
+	if (fork() == 0)
 	{
-		process_wait();
+		dup2(pipes[1], 1);
+		close(pipes[0]);
+		close(pipes[1]);
+		if (fork() == 0)
+		{
+			execl(pr1, NULL);
+		}
+		else
+		{
+			wait(NULL);
+			if (fork() == 0)
+			{
+				int fd = open(a1, O_RDONLY);
+				dup2(fd, 0);
+				close(fd);
+				execl(pr2, NULL);
+			}
+			else
+			{
+				wait(NULL);
+			}
+		}
 	}
-	process_start(pr2);
-	close(a1); close(a3);
-	close(pipes[0]); close(pipes[1]);
-	while (pr2->state != EXITED || pr3->state != EXITED)
+	else
 	{
-		process_wait();
+		if (fork() == 0)
+		{
+			dup2(pipes[0], 0);
+			close(pipes[0]);
+			close(pipes[1]);
+			int fd = creat(a3, 0777);
+			dup2(fd, 1);
+			close(fd);
+			execl(pr3, a2, NULL);
+		}
+		else
+		{
+			wait(NULL);
+			wait(NULL);
+		}
 	}
-	process_delete(pr1);
-	process_delete(pr2);
-	process_delete(pr3);
-	process_deinit();
 	return 0;
 }
