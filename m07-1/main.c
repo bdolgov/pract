00af257c8fd4e2fc2f31599764b4c621c04f2a38
Boris Dolgov <boris@dolgov.name>
2012-11-30 17:09:20 +0400

    FUUUUUUUUUUUUUUU execlP

--- m07-1/main.c
+++ m07-1/main.c
@@ -17,46 +17,30 @@ int main(int ac, char** av)
 		close(pipes[1]);
 		if (fork() == 0)
 		{
-			execl(pr1, pr1, NULL);
+			execlp(pr1, pr1, NULL);
 			return 127;
-		}
-		else
-		{
-			wait(NULL);
-			if (fork() == 0)
-			{
-				int fd = open(a1, O_RDONLY);
-				dup2(fd, 0);
-				close(fd);
-				execl(pr2, pr2, NULL);
-				return 127;
-			}
-			else
-			{
-				wait(NULL);
-			}
-		}
-	}
-	else
-	{
-		if (fork() == 0)
-		{
-			dup2(pipes[0], 0);
-			close(pipes[0]);
-			close(pipes[1]);
-			int fd = creat(a3, 0777);
-			dup2(fd, 1);
-			close(fd);
-			execl(pr3, pr3, a2, NULL);
-			return 127;
-		}
-		else
-		{
-			close(pipes[0]);
-			close(pipes[1]);
-			wait(NULL);
 		}
 		wait(NULL);
+		int fd = open(a1, O_RDONLY);
+		dup2(fd, 0);
+		close(fd);
+		execlp(pr2, pr2, NULL);
+		return 127;
 	}
+	if (fork() == 0)
+	{
+		dup2(pipes[0], 0);
+		close(pipes[0]);
+		close(pipes[1]);
+		int fd = creat(a3, 0644);
+		dup2(fd, 1);
+		close(fd);
+		execlp(pr3, pr3, a2, NULL);
+		return 127;
+	}
+	close(pipes[0]);
+	close(pipes[1]);
+	wait(NULL);
+	wait(NULL);
 	return 0;
 }
